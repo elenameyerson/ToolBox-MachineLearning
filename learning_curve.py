1f52e4609feae6e1d264438f29fd39c26f036b58
elenameyerson <elena.meyerson@students.olin.edu>
2017-03-13 23:50:44 -0400

    Submitting machine learning toolbox

--- learning_curve.py
+++ learning_curve.py
@@ -2,6 +2,7 @@
 
 import matplotlib.pyplot as plt
 import numpy
+import scipy
 from sklearn.datasets import *
 from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LogisticRegression
@@ -22,7 +23,8 @@ def train_model():
     data = load_digits()
     num_trials = 10
     train_percentages = range(5, 95, 5)
-    test_accuracies = numpy.zeros(len(train_percentages))
+    test_accuracies = []
+    data = load_digits()
 
     # train models with training percentages between 5 and 90 (see
     # train_percentages) and evaluate the resultant accuracy for each.
@@ -32,6 +34,18 @@ def train_model():
     # model = LogisticRegression(C=10**-10) for your learner
 
     # TODO: your code here
+    for i in train_percentages:
+        all_ys = []
+        for j in range(num_trials):
+            X_train, X_test, y_train, y_test = train_test_split(data.data, data.target,
+            train_size = i)
+            model = LogisticRegression(C=1**-50)
+            model.fit(X_train, y_train)
+            all_ys.append(model.score(X_test, y_test))
+            #print("Train accuracy %f" %model.score(X_train, y_train))
+            #print("Test accuracy %f"%model.score(X_test, y_test))
+        avg_y = sum(all_ys)/len(all_ys)
+        test_accuracies.append(avg_y)
 
     fig = plt.figure()
     plt.plot(train_percentages, test_accuracies)
@@ -42,5 +56,6 @@ def train_model():
 
 if __name__ == "__main__":
     # Feel free to comment/uncomment as needed
-    display_digits()
-    # train_model()
+    #display_digits()
+
+    train_model()
